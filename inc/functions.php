2ef644ad72df9a11593227b3fcdb340f99c041c6
Lene <lenecarpertunnel@gmail.com>
2023-03-20 11:58:56 +0100

    Update SQL queries

--- inc/functions.php
+++ inc/functions.php
@@ -1,5 +1,30 @@
 <?php
+    function db()
+    { // Connect to the MySQL database
+    $mysqli = new mysqli('localhost', 'root', '', 'rocmemo');
 
-    $db = mysqli_connect('localhost', 'root', '', 'rocmemo')
+    // Checks the connection
+    if($mysqli -> connect_errno)
+    {
+        echo "Connection failed " . $mysqli -> connect_error;
+        exit();
+    }
 
+    // Return the database
+    return $mysqli;
+    }
+
+    # Function to retrieve and upload the data into the database
+    function uploadScore()
+    {
+    # Define variables to store data in and connect to the SQL database
+    $db = mysqli_connect('localhost', 'root', '', 'rocmemo');
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $time = mysqli_real_escape_string($db, "Time here");
+    $score = mysqli_real_escape_string($db, "Score here");
+    # Gather all the data into an SQL query
+    $upload = "INSERT into highscores (username, time, clicks) VALUES ($username, $time, $score)";
+    # Query the data to be sent into the corresponding database tables
+    $query = $db->query($upload) or die($db->error);
+    }
 ?>
